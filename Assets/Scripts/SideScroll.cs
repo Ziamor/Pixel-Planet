097a6c2b48ef2d421410f616bc5547990d2fe470
Alex Zukowski <ziamor23@gmail.com>
2019-08-18 17:43:38 -0400

    Added snap swiping

--- Assets/Scripts/SideScroll.cs
+++ Assets/Scripts/SideScroll.cs
@@ -1,30 +1,58 @@
 ﻿using UnityEngine;
 
 public class SideScroll : MonoBehaviour {
+    public GameObject[] planets;
+
     public float scrollSpeed = 1f;
     public float smoothTime = 0.3F;
 
     Vector2 lastPos;
     Vector3 scrollVelocity = Vector3.zero;
 
+    Vector3 startPos;
+
+    int index = 0;
+
+    float swipeTime = 0;
+
     // Start is called before the first frame update
     void Start() {
         lastPos = Input.mousePosition;
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update() {
         Vector2 currentPosition = Input.mousePosition;
-        Vector3 target = transform.position;
 
-        if (Input.GetMouseButton(0)) {
+
+        if (Time.time - swipeTime >= smoothTime && Input.GetMouseButton(0)) {
+            swipeTime = Time.time;
             Vector3 swipe = (currentPosition - lastPos);
             float magnitude = swipe.magnitude;
-            target += Vector3.right * -Mathf.Sign(swipe.x) * scrollSpeed * Mathf.Clamp01(magnitude);
+            if (swipe.x != 0 && magnitude > 0.01f) {
+                int newIndex = index;
+                if (swipe.x < 0) {
+                    newIndex++;
+                } else {
+                    newIndex--;
+                }
+
+                if (IsValidIndex(newIndex)) {
+                    index = newIndex;
+                }
+            }
+        }
+        if (IsValidIndex(index)) {
+            Vector3 target = planets[index].transform.position;
+            target.z = startPos.z;
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref scrollVelocity, smoothTime);
         }
 
-        transform.position = Vector3.SmoothDamp(transform.position, target, ref scrollVelocity, smoothTime);
-
         lastPos = currentPosition;
+    }
+
+    bool IsValidIndex(int i) {
+        return i >= 0 && i <= planets.Length - 1 && planets[i] != null;
     }
 }
